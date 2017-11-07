37c1818da5cacc4d8a8c86eec20042f9ae5b64a0
Philip-Nicolas <phil.nic@tuta.io>
2017-11-07 17:04:42 -0500

    Added BT Unsupported message.

--- bolt/ConnectionVC.swift
+++ bolt/ConnectionVC.swift
@@ -11,31 +11,44 @@ import CoreBluetooth
 class ConnectionVC: UIViewController, CBCentralManagerDelegate {
     
     override func viewDidLoad() {
-        super.viewDidLoad()
+        super.viewDidLoad ()
     }
     
-    override func viewDidAppear(_ animated: Bool) {
-        //enableBTAlert()
+    override func viewDidAppear (_ animated: Bool) {
     }
     
-    func centralManagerDidUpdateState(_ central: CBCentralManager) {
+    func centralManagerDidUpdateState (_ central: CBCentralManager) {
         switch (central.state) {
-        case CBManagerState.poweredOff:
-            print("bluetooth is off")
-            break
         case CBManagerState.poweredOn:
-            print("bluetooth is on")
+            break
+        case CBManagerState.poweredOff:
+            sendBTOffAlert ()
+            break
+        case CBManagerState.unsupported:
+            sendBTUnsupportedAlert ()
+            break
         default: break
         }
     }
     
-    func enableBTAlert() {
-        let alert = UIAlertController (title: "Bluetooth Off", message: "Please enable Bluetooth in Settings", preferredStyle:UIAlertControllerStyle.alert)
+    func sendBTUnsupportedAlert () {
+        let alert = UIAlertController (title: "Bluetooth Unsupported", message: "Bluetooth is unsupported on your device. Sorry.", preferredStyle:UIAlertControllerStyle.alert)
         
-        alert.addAction(UIAlertAction(title: "Go to Settings", style: UIAlertActionStyle.default, handler: { (action) in
-            alert.dismiss(animated: true, completion: nil)
+        alert.addAction (UIAlertAction (title: "Ok", style: UIAlertActionStyle.default, handler: nil))
+        alert.dismiss(animated: true, completion: nil)
+    }
+    
+    func sendBTOffAlert () {
+        let alert = UIAlertController (title: "Bluetooth Off", message: "Please enable Bluetooth in Settings, or in the Control Center.", preferredStyle:UIAlertControllerStyle.alert)
+        
+        alert.addAction (UIAlertAction (title: "Ok", style: UIAlertActionStyle.default, handler: { (action) in
+            alert.dismiss (animated: true, completion: nil)
         }))
         
-        self.present(alert, animated: true, completion: nil)
+        alert.addAction (UIAlertAction (title: "Go to Settings", style: UIAlertActionStyle.default, handler: { (action) in
+            alert.dismiss (animated: true, completion: nil)
+        }))
+        
+        self.present (alert, animated: true, completion: nil)
     }
 }
